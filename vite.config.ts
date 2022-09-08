a13e190f96f9eb6c0e02c83a3c77ab1e0fcce0ee
migbash <20924663+migbash@users.noreply.github.com>
2022-09-08 19:38:04 +0100

    updating to cause `error`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [chunkSplitPlugin(), sveltekit()]
 };
 
 export default config;
